842eb98f1e5cc048b0dda72199eeffe4500248ee
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2021-06-24 21:39:30 +0530

    Home Page added

---
+++ app/src/main/java/com/mtdaps/obdfuel/activities/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.mtdaps.obdfuel.activities.bluetooth1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mtdaps.obdfuel.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
