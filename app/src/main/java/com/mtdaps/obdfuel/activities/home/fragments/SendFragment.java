4245b93801107b807abb1b3f293d8961df018c74
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2021-07-07 20:58:08 +0530

    send obd and cancel button is set

--- app/src/main/java/com/mtdaps/obdfuel/activities/home/fragments/SendFragment.java
+++ app/src/main/java/com/mtdaps/obdfuel/activities/home/fragments/SendFragment.java
@@ -3,6 +3,7 @@ package com.mtdaps.obdfuel.activities.home.fragments;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -10,15 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.transition.Visibility;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mtdaps.obdfuel.R;
 import com.mtdaps.obdfuel.util.ActivityInterface;
+import com.mtdaps.obdfuel.util.UiUtil;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -29,8 +37,13 @@ import static androidx.core.content.ContextCompat.getSystemService;
  */
 public class SendFragment extends Fragment implements ActivityInterface {
 
+    private ViewGroup container;
     private TextInputLayout vehicleName;
     private TextInputEditText vehicleNameEditText;
+    private Button sendObdDataButton, cancelButton;
+    private TextView sendText;
+    private ProgressBar progressBar;
+    private ConstraintLayout sendFragmentLayout;
 
     private ConnectivityManager connectivityManager;
 
@@ -43,6 +56,7 @@ public class SendFragment extends Fragment implements ActivityInterface {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        this.container = container;
         return inflater.inflate(R.layout.fragment_send, container, false);
     }
 
@@ -58,8 +72,16 @@ public class SendFragment extends Fragment implements ActivityInterface {
     public void setup() {
         vehicleName = getView().findViewById(R.id.vehicleName);
         vehicleNameEditText = getView().findViewById(R.id.vehicleNameEditText);
+        sendObdDataButton = getView().findViewById(R.id.sendObdDataButton);
+        cancelButton = getView().findViewById(R.id.cancelButton);
+        sendText = getView().findViewById(R.id.sendText);
+        progressBar = getView().findViewById(R.id.progressBar);
+        sendFragmentLayout = getView().findViewById(R.id.sendFragmentLayout);
 
         addTextChangedListener(vehicleNameEditText, vehicleName);
+
+        sendObdDataButtonOnClickListner();
+        cancelButtonOnClickListner();
     }
 
     /**
@@ -67,7 +89,7 @@ public class SendFragment extends Fragment implements ActivityInterface {
      * @param e
      * @param t
      */
-    private static void addTextChangedListener(TextInputEditText e, final TextInputLayout t) {
+    private void addTextChangedListener(TextInputEditText e, final TextInputLayout t) {
         e.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -77,7 +99,7 @@ public class SendFragment extends Fragment implements ActivityInterface {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (s.toString().trim().length() < 6) {
-                    t.setError("Vehicle Name should be at least 6 characters");
+                    t.setError(getResources().getString(R.string.vehicle_name_error));
                 } else {
 
                     t.setError("");
@@ -89,6 +111,48 @@ public class SendFragment extends Fragment implements ActivityInterface {
             public void afterTextChanged(Editable editable) {
 
             }
+        });
+    }
+
+    private void switchVisibilityOnSendingObdData(){
+        int visibility = sendText.getVisibility();
+        if(visibility == View.GONE){
+            sendText.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+            cancelButton.setVisibility(View.VISIBLE);
+            sendObdDataButton.setVisibility(View.GONE);
+            vehicleName.setEnabled(false);
+        } else{
+            sendText.setVisibility(View.GONE);
+            progressBar.setVisibility(View.GONE);
+            cancelButton.setVisibility(View.GONE);
+            sendObdDataButton.setVisibility(View.VISIBLE);
+            vehicleName.setEnabled(true);
+        }
+
+
+    }
+
+    private void sendObdDataButtonOnClickListner(){
+        sendObdDataButton.setOnClickListener(view -> {
+            if(isInternetAvailable()){
+                if(vehicleNameEditText.getText().toString().trim().equals("")){
+                    vehicleName.setError(getResources().getString(R.string.vehicle_name_error));
+                } else{
+                    switchVisibilityOnSendingObdData();
+                }
+            } else{
+                Toast.makeText(getContext(),"Internet is not Connected",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void cancelButtonOnClickListner(){
+        cancelButton.setOnClickListener(view -> {
+            // TODO: cancel the process
+            switchVisibilityOnSendingObdData();
+            Toast.makeText(getContext(),"Sending OBD Data Canceled",Toast.LENGTH_LONG).show();
+
         });
     }
 
